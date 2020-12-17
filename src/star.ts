d061a817ba89bd6f2856f9501ac7efb422bb3f71
lsoder <lindaevasoder@gmail.com>
2020-12-17 13:48:01 +0100

    added some more func on stars and added update function in gamegui

--- src/star.ts
+++ src/star.ts
@@ -5,18 +5,18 @@ class Star extends GameObject {
         super();
         this.opacity = random(100, 200);
         this.position = createVector(random(width), random(height));
-        this.velocity = createVector(0.5, 1);
+        this.velocity = createVector(0.2, 0);
         this.acceleration = createVector(50, 50);
     }
 
     public draw() {
         stroke(color(230, 255, 255, this.opacity));
-        ellipse(this.position.x,this.position.y, random(0.2, 0.8));
+        ellipse(this.position.x,this.position.y, random(0.2, width/1000));
 
     }
 
     public update() {
-        this.position.add(this.velocity);
+        this.position.sub(this.velocity);
         console.log(this.velocity);
     }
 }
