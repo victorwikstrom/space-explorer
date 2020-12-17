2d4695048998ff4dba639ab2df83c27b285cb9bb
lsoder <lindaevasoder@gmail.com>
2020-12-17 21:34:08 +0100

    add function that create new stars

--- src/star.ts
+++ src/star.ts
@@ -5,17 +5,21 @@ class Star extends GameObject {
     super();
     this.opacity = random(100, 200);
     this.position = createVector(random(width), random(height));
-    this.velocity = createVector(0.2, 0);
+    this.velocity = createVector(0.5, 0);
     this.acceleration = createVector(50, 50);
   }
 
   public draw() {
+    strokeWeight(1);
     stroke(color(230, 255, 255, this.opacity));
-    ellipse(this.position.x, this.position.y, random(0.2, width / 1000));
+    ellipse(this.position.x, this.position.y, random(0.1, width / 900));
   }
 
   public update() {
     this.position.sub(this.velocity);
-    console.log(this.velocity);
+  
+    if(this.position.x < 0) {
+      this.position.x = width + 1;
+    }
   }
 }
