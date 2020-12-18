a535a76fef633d9db874fead63a99e080506f0c4
victorwikstrom <victor.wikstrom@icloud.com>
2020-12-18 13:55:09 +0100

    Fixed the prestanda even more. SUPER SMOOTH

--- src/star.ts
+++ src/star.ts
@@ -3,25 +3,32 @@ class Star extends GameObject {
   y: number;
   xspeed: number;
   private opacity: number;
-  private color: p5.Color;
+  private color: number;
 
   constructor() {
     super();
     this.opacity = random(100, 200);
     this.x = random(width);
     this.y = random(height);
-    this.xspeed = random(0.2, 0.5);
-    this.color = color(230, 255, 255, this.opacity);
+    this.xspeed = random(0.5, 1);
+    (this.color = random(229, 255)), 255, 255;
   }
 
   public draw() {
     strokeWeight(1);
-    stroke(this.color);
-    circle(this.x, this.y, random(0.1, width / 900));
+
+    if (this.xspeed < 0.7) {
+      this.opacity = random(50, 100);
+      strokeWeight(1.5);
+    } else if (this.xspeed > 0.9) {
+      strokeWeight(3);
+    }
+    line(this.x, this.y, this.x, this.y);
+    stroke(this.color, this.opacity);
   }
 
   public update() {
-    this.x - this.xspeed;
+    this.x -= this.xspeed;
     if (this.x < 0) {
       this.x = width + 1;
     }
