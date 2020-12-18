c8414b87ddb125f34429659f23220abcbc9ac466
victorwikstrom <victor.wikstrom@icloud.com>
2020-12-18 11:53:36 +0100

    Add one array of gameobjects instead of separate arrays

--- src/gameplay.ts
+++ src/gameplay.ts
@@ -3,73 +3,36 @@ class GamePlay {
   private player: Player;
   private isActive: boolean;
   public button: p5.Element;
-  private stars: Array<Star>;
-  private spacediamonds: Array<Spacediamond>; // THIS ARRAY IS GOING TO BE A GAME OBJECTS ARRAY INSTEAD. 
-  private meteorites: Array<Meteorite>;// THIS ARRAY IS GOING TO BE A GAME OBJECTS ARRAY INSTEAD. 
-  private blackholes: Array<BlackHole>
+  private gameObjects: Array<GameObject>;
 
   constructor(gameGUI: IGameState) {
     this.gameGUI = gameGUI;
     this.isActive = false;
     this.button = createButton("Go to GameOver GUI");
-    this.stars = [];
+    this.gameObjects = [];
     this.player = new Player();
-    this.spacediamonds = [];
-    this.meteorites = [];
-    this.blackholes = [];
   }
 
   public update() {
     this.player.update();
+    this.updateGameObjects();
     this.button.mousePressed(this.changeGui);
-
-    for (let star of this.stars) {
-      star.update();
-    }
-    for (let spacediamond of this.spacediamonds) {
-      spacediamond.update();
-    }
-    for (let meteorite of this.meteorites) {
-      meteorite.update();
-    }
-
-    for (let blackhole of this.blackholes){
-      blackhole.update();
-    }
-
   }
 
   public draw() {
     // GUI SETUP
     if (this.isActive === false) {
-      this.createStars();
-      this.createSpaceDiamonds();
-      this.createMeteorite();
-      this.createBlackHoles();
+      this.createGameObjects();
       this.isActive = true;
     }
 
-    for (let star of this.stars) {
-      star.draw();
-    }
-
-    for (let spacediamond of this.spacediamonds) {
-      spacediamond.draw();
-    }
-
-    for (let meteorite of this.meteorites) {
-      meteorite.draw();
-    }
-
-    for (let blackhole of this.blackholes) {
-      blackhole.draw();
-    }
-
-    this.createElements();
+    this.createGuiButtonAndText(); // NEEDS TO BE DRAWN ALL THE TIME
     this.player.draw();
+    this.drawGameObjects();
   }
 
-  private createElements() {
+  /** Creates button and text for changing gui */
+  private createGuiButtonAndText() {
     // CREATE TEXT
     fill("white");
     textSize(30);
@@ -81,32 +44,66 @@ class GamePlay {
     this.button.position(10, 50);
   }
 
+  /** Change gui to Game Over */
   private changeGui = () => {
     this.gameGUI.updateGUI("over");
   };
 
-  private createStars() {
+  /** Create all game object instances */
+  private createGameObjects() {
     for (let i = 0; i < 1000; i++) {
-      this.stars[i] = new Star();
+      this.gameObjects.push(new Star());
     }
-  }
-
-  private createSpaceDiamonds() {
     for (let i = 0; i < 5; i++) {
-      this.spacediamonds[i] = new Spacediamond();
+      this.gameObjects.push(new Spacediamond());
     }
-  }
-
-  private createMeteorite() {
     for (let i = 0; i < 3; i++) {
-      this.meteorites[i] = new Meteorite();
+      this.gameObjects.push(new Meteorite());
     }
-  }
-  
-  private createBlackHoles() {
     for (let i = 0; i < 4; i++) {
-      this.blackholes[i] = new BlackHole();
+      this.gameObjects.push(new BlackHole());
     }
   }
 
+  /** Call draw() on all gameObjects */
+  private drawGameObjects() {
+    for (let gameObject of this.gameObjects) {
+      if (gameObject instanceof Star) {
+        gameObject.draw();
+      }
+      if (gameObject instanceof Planet) {
+        gameObject.draw();
+      }
+      if (gameObject instanceof Meteorite) {
+        gameObject.draw();
+      }
+      if (gameObject instanceof Spacediamond) {
+        gameObject.draw();
+      }
+      if (gameObject instanceof BlackHole) {
+        gameObject.draw();
+      }
+    }
+  }
+
+  /** Call update() on all gameObjects */
+  private updateGameObjects() {
+    for (let gameObject of this.gameObjects) {
+      if (gameObject instanceof Star) {
+        gameObject.update();
+      }
+      if (gameObject instanceof Planet) {
+        gameObject.update();
+      }
+      if (gameObject instanceof Meteorite) {
+        gameObject.update();
+      }
+      if (gameObject instanceof Spacediamond) {
+        gameObject.update();
+      }
+      if (gameObject instanceof BlackHole) {
+        gameObject.update();
+      }
+    }
+  }
 }
