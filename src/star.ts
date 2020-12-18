0edfa15bec415133bc831e259352cbc16c137586
victorwikstrom <victor.wikstrom@icloud.com>
2020-12-18 13:47:52 +0100

    Fixed some prestanda issues for rendering stars

--- src/star.ts
+++ src/star.ts
@@ -1,24 +1,29 @@
 class Star extends GameObject {
+  x: number;
+  y: number;
+  xspeed: number;
   private opacity: number;
+  private color: p5.Color;
 
   constructor() {
     super();
     this.opacity = random(100, 200);
-    this.position = createVector(random(width), random(height));
-    this.velocity = createVector(0.5, 0);
-    this.acceleration = createVector(50, 50);
+    this.x = random(width);
+    this.y = random(height);
+    this.xspeed = random(0.2, 0.5);
+    this.color = color(230, 255, 255, this.opacity);
   }
 
   public draw() {
     strokeWeight(1);
-    stroke(color(230, 255, 255, this.opacity));
-    ellipse(this.position.x, this.position.y, random(0.1, width / 900));
+    stroke(this.color);
+    circle(this.x, this.y, random(0.1, width / 900));
   }
 
   public update() {
-    this.position.sub(this.velocity);
-    if(this.position.x < 0) {
-      this.position.x = width + 1;
+    this.x - this.xspeed;
+    if (this.x < 0) {
+      this.x = width + 1;
     }
   }
 }
