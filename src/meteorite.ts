3b95bb5ec023a2ac7566ea6258727c120c66d56a
lsoder <lindaevasoder@gmail.com>
2020-12-18 09:52:03 +0100

    added class object for meteorite

---
+++ src/meteorite.ts
@@ -0,0 +1,26 @@
+class Meteorite extends GameObject {
+    damage: number;
+    // private collisionSound: string - To be added later
+
+    constructor() {
+        super();
+        this.size = 0;
+        this.position = createVector(0, 0);
+        this.velocity = createVector(0, 0);
+    // this.acceleration = createVector(0, 0);
+        this.damage = 0;
+    // this.collisionSound = p5.SoundFile;        
+    }
+
+    update() {
+        
+    }
+
+    draw() {
+
+    }
+
+    
+
+    
+}
