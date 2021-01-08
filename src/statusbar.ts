7ac05965316686b88cc46f1d5c96af4ddeed1e84
feliciaseg <felicia.segerholm97@gmail.com>
2021-01-08 13:49:36 +0100

    update spaceship condition + distancefromearth

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -3,47 +3,69 @@ class StatusBar {
     textColor: p5.Color;
     stroke: p5.Color;
     ellipses: Array<any>
+    distanceFromEarth: number;
+    gameVelocity: number;
 
     constructor() {
         this.position = createVector(0, 0);
         this.textColor = color("white");
         this.stroke = color("blue");
         this.ellipses = Array<any>()
+        this.distanceFromEarth = 0;
+        this.gameVelocity = 1;
     }
 
-    update() {
-
+    update(gameAcceleration:number) {
+        this.gameVelocity += gameAcceleration
+        this.distanceFromEarth += deltaTime/800 * this.gameVelocity;
+        
     }
 
-    draw() {
-    push();
-    strokeWeight(4);
-    stroke(this.stroke);
-    fill(0);
-    rect(0, 0, width/3, 70);
-    rect(width/3, 0, width/3, 70);
-    rect(width-width/3, 0, width/3, 70);
-    pop();
+    draw(health: number) {
+        push();
+        strokeWeight(4);
+        stroke(this.stroke);
+        fill(0);
+        rect(0, 0, width/3, 70);
+        rect(width/3, 0, width/3, 70);
+        rect(width-width/3, 0, width/3, 70);
+        pop();
 
-    push();
-    textSize(20);
-    noStroke();
-    fill(this.textColor);
-    // DISTANCE FROM EARTH
-    text("DISTANCE FROM EARTH", 70, 30);
-    text("NUMBER", 70, 50);
-    // CURRENT SPEED
-    text("CURRENT SPEED", width/3+30, 30);
-    text("NUMBER", width/3+30, 50);
-    // SPACESHIP CONDITION
-    text("SPACESHIP CONDITION", width-width/3+30, 30);
-    this.drawEllipses();
-    pop();
+        push();
+        textSize(20);
+        noStroke();
+        fill(this.textColor);
+        // DISTANCE FROM EARTH
+        text("DISTANCE FROM EARTH", 70, 30);
+        text(this.distanceFromEarth.toFixed() + " 000", 70, 50);
+        // CURRENT SPEED
+        text("CURRENT SPEED", width/3+30, 30);
+        text("NUMBER", width/3+30, 50);
+        // SPACESHIP CONDITION
+        text("SPACESHIP CONDITION", width-width/3+30, 30);
+        this.drawEllipses(health);
+        pop();
     }
 
-    drawEllipses(){
-        for (let x = 35; x <235; x = x+20){
-            this.ellipses = [ellipse(width-width/3+x, 50, 10, 10)]
+    drawEllipses(health: number) {
+        for (let i = 0; i < health; i++) {
+            const x = width-width/3 + 35 + i * 20;
+            this.ellipses.push(ellipse(x, 50, 10, 10))
         }
+
+        // this.ellipses = [
+        // ellipse(width-width/3+35, 50, 10, 10),
+        // ellipse(width-width/3+55, 50, 10, 10),
+        // ellipse(width-width/3+75, 50, 10, 10),
+        // ellipse(width-width/3+95, 50, 10, 10),
+        // ellipse(width-width/3+105, 50, 10, 10),
+        // ellipse(width-width/3+115, 50, 10, 10),
+        // ellipse(width-width/3+135, 50, 10, 10),
+        // ellipse(width-width/3+155, 50, 10, 10),
+        // ellipse(width-width/3+175, 50, 10, 10),
+        // ellipse(width-width/3+195, 50, 10, 10),
+        // ellipse(width-width/3+215, 50, 10, 10),
+        // ]
+
     }
 }
