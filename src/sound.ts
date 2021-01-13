cfa6680cc3a00b85bc0bf3818da50eb3d953ed26
Camilla <camilla.johansson1986@gmail.com>
2021-01-13 11:29:56 +0100

    sound play

--- src/sound.ts
+++ src/sound.ts
@@ -1,58 +1,63 @@
 class Sound {
-  //   soundtrack: p5.SoundFile;
-  public audioButton: p5.Element;
-  public muteIcon: p5.Image;
-  public soundIcon: p5.Image;
-  // gameintro: p5.SoundFile;
+      // public soundtrack: p5.SoundFile;
+      public audioButton: p5.Element;
+      public muteIcon: p5.Image;
+      public soundIcon: p5.Image;
+      // gameintro: p5.SoundFile;
+      // gameplay: p5.SoundFile;
+      // gameover: p5.SoundFile;
+      // shot: p5.SoundFile;
+      // collision: p5.SoundFile;
+      // blackhole: p5.SoundFile;
+      // explode: p5.SoundFile;
+    
+      constructor() {
+        this.audioButton = createImg("assets/images/audio-8.png");
+        this.muteIcon = this.setMuteIcon(muteIcon);
+        this.soundIcon = this.setSoundIcon(soundIcon);
+        this.audioButton.mousePressed(this.toggleSound);
+        // this.audioButton = createButton('mute');
+        //       soundtrack = loadSound(
+        //       "assets/sound/Fair_Use_Trio_-_06_-_2001_A_Space_Odyssey.mp3"
+        //     );
+      }
+    
+      update() {
+        this.toggleSound();
+        this.audioButton;
+        this.audioButton.mousePressed(this.toggleSound);  
+      }
+    
+      draw() {
+        this.audioButton.show();
+        this.audioButton = createImg("assets/images/audio-8.png");
+        this.audioButton.style("background-color", "blue");
+        this.audioButton.style("border-radius", "50%");
+        this.audioButton.position(windowWidth - 80, 12);
+      }
+    
+      public setMuteIcon(muteIcon: p5.Image) {
+          return muteIcon;
+      }
+    
+      public setSoundIcon(soundIcon: p5.Image) {
+          return soundIcon;
+      }
+    
+      public toggleSound() {
+        if (soundtrack.isPlaying()) {
+          soundtrack.pause();
+          // this.audioButton.html("pause");
+      //     this.audioButton = createImg("assets/images/sound-on.svg");
+        } else {
+          soundtrack.play();
+          // this.audioButton.html("play");
+      //     this.audioButton = createImg("assets/images/sound-off.svg");
+        }
+      }
 
-  // gameplay: p5.SoundFile;
-  // shot: p5.SoundFile;
-  // collision: p5.SoundFile;
-  // blackhole: p5.SoundFile;
-  // explode: p5.SoundFile;
-
-  constructor() {
-    this.audioButton = createImg("assets/images/sound-off.svg");
-    this.muteIcon = this.setMuteIcon(muteIcon);
-    this.soundIcon = this.setSoundIcon(soundIcon);
-    this.audioButton.mousePressed(this.toggleSound);
-    // this.audioButton = createButton('mute');
-    //       soundtrack = loadSound(
-    //       "assets/sound/Fair_Use_Trio_-_06_-_2001_A_Space_Odyssey.mp3"
-    //     );
-  }
-
-  update() {
-    this.toggleSound();
-    this.audioButton;
-    this.audioButton.mousePressed(this.toggleSound);  
-  }
-
-  draw() {
-    this.audioButton.show();
-    this.audioButton = createImg("assets/images/audio-8.png");
-    this.audioButton.style("background-color", "blue");
-    this.audioButton.style("border-radius", "50%");
-    this.audioButton.position(windowWidth - 80, 12);
-  }
-
-  public setMuteIcon(muteIcon: p5.Image) {
-      return muteIcon;
-  }
-
-  public setSoundIcon(soundIcon: p5.Image) {
-      return soundIcon;
-  }
-
-  public toggleSound() {
-    if (soundtrack.isPlaying()) {
-      soundtrack.pause();
-      // this.audioButton.html("pause");
-      this.audioButton = createImg("assets/images/sound-on.svg");
-    } else {
-      soundtrack.play();
-      // this.audioButton.html("play");
-      this.audioButton = createImg("assets/images/sound-off.svg");
+      public sound(sound: p5.SoundFile) {
+            sound.play();
+      }
     }
-  }
-}
+    
